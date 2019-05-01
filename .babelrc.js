7d1676ceec109f3870f295690c060c935045c371
Ajay Poshak <ajay.poshak@bookmyshow.com>
2019-05-01 17:40:23 +0530

    Create babel config to publish esm and cjs modules

---
+++ .babelrc.js
@@ -0,0 +1,22 @@
+/* if no targets are specified, @babel/preset-env
+ * behaves exactly the same as @babel/preset-es2015, @babel/preset-es2016 and @babel/preset-es2017 together
+ */
+module.exports = {
+	presets: ['@babel/preset-env', '@babel/preset-react'],
+	plugins: [
+		'@babel/plugin-transform-runtime',
+		'@babel/plugin-proposal-object-rest-spread',
+		'@babel/proposal-class-properties',
+	],
+	env: {
+		// Configuration for ES Modules
+		esm: {
+			presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
+			plugins: [
+				['@babel/plugin-transform-runtime', { useESModules: true }],
+				'@babel/plugin-proposal-object-rest-spread',
+				'@babel/proposal-class-properties',
+			],
+		},
+	},
+}
