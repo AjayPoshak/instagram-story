6a62a8f8206ff306a23e541af25bea596e2f2a2a
Ajay Poshak <poshakajay@gmail.com>
2019-05-04 10:59:00 +0530

    Create rollup config

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import babel from 'rollup-plugin-babel';
+import css from 'rollup-plugin-css-only'
+
+export default {
+  input: './src/index.js',
+  output: [
+    {
+      file: 'dist/cjs/story.js',
+      format: 'cjs',
+    }, {
+      file: 'dist/esm/story.js',
+      format: 'esm',
+    }],
+  plugins: [
+    babel({
+      runtimeHelpers: true,
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+    }),
+    css({ output: 'App.css' }),
+  ],
+}
